de26c7adfb37ae1975334879fde6925e27a4ad22
labe0653 <7586428+landonbedell@users.noreply.github.com>
2022-06-04 15:08:26 -0600

    fetch mars image data from pub api

---
+++ src/services/apiService.ts
@@ -0,0 +1,13 @@
+const marsRoverImageryCache: any = {};
+const fetchMarsRoverImagery = async (page: number = 0) => {
+  if (marsRoverImageryCache[page]) {
+    return marsRoverImageryCache[page];
+  }
+  const res: any = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=${page}&api_key=DEMO_KEY`)
+  const { photos } = await res.json();
+  marsRoverImageryCache[page] = photos;
+  console.log('photos', photos);
+  return photos;
+};
+
+export default fetchMarsRoverImagery;
